2455d0e596da6f49c1068ac01062faf3430644a9
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-20 23:43:49 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,6 +37,20 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
+  
+  	monopoly = {:railroads =>
+		{:pieces =>4,
+		 :rent_in_dollars => { 
+		   :one_piece_owned =>25,
+      :two_pieces_owned =>50,
+      :three_pieces_owned =>100,
+      :four_pieces_owned =>200},
+      :names => {
+        :reading_raildroad => {
+          "mortgage_value" => "$100"},
+          :pennsylvania_raildroad => { "mortgage_value" => "$200"},
+          :b_and_o_railroad => { "mortgage_value" => "$400"},
+          :shortline => { "mortgage_value" => "$800"}
 	
 end
 
