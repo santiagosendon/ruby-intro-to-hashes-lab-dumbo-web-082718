d043bac456b995767a18abd051ecb0197dc00a8b
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-20 23:30:21 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,10 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-		monopoly = {:railroads => {:pieces =>4, :names}
+		monopoly = {:railroads => {:pieces =>4, 
+
+
+
 end
 
 def monopoly_with_third_tier
